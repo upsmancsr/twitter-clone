e03f436e461f3c09447ec688b4532f23f69bd5e6
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-09-24 20:24:33 -0700

    Modified server.js to utilized http.createServer and socket.io.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,34 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-const server = express();
-server.use(express.json());
-server.use(cors());
+const app = express();
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+io.on("connection", socket => {
+    stream();
+    socket.on("connection", () => console.log("Client connected"));
+    socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
+//Emits data with socket.io as twitter stream flows in
+const stream = () => {
+   twitter.stream('statuses/filter', { track: app.locals.searchTerm }, (stream) => {
+       stream.on('data', (tweet) => {
+           sendMessage(tweet);
+       });
+
+       stream.on('error', (error) => {
+           console.log(error);
+       });
+   });
+}
 
 server.get('/',(req, res) => {
     res.send("Server base URL is working...");
