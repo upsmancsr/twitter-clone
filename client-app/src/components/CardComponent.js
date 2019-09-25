3562892a060d72f5d61558f60a6fc1395e0f0d49
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-09-25 00:29:47 -0700

    card components scss styles added and some mixins.

--- client-app/src/components/CardComponent.js
+++ client-app/src/components/CardComponent.js
@@ -1,26 +1,30 @@
 import React from 'react';
+import '../scss/main.scss';
 
 class CardComponent extends React.Component {
     render() {
         let data = this.props.data;
 
         return (
-            <div>
-                <div className="card-panel grey lighten-5 z-depth-3 hoverable thin">
-                    <div className="row valign-wrapper">
-                        <div className="col s2">
-                            <img src={data.user.profile_image_url} alt={data.user.name} className="circle responsive-img" />
+            <div className='card'>
+                <div className='card-profile-image'>
+                    <img src={data.user.profile_image_url} alt={data.user.name} className='circle responsive-img' />
+                </div> 
+
+                <div className='card-content'>
+                    <div className='tweet-header'> 
+                        <div className='tweet-author'>
+                            <a href={`https://twitter.com/${data.user.screen_name}`} target='_blank'>{`@${data.user.screen_name}`}</a>
                         </div>
-                        <div className="col s10 left-align">
-                            <span className="black-text">{data.text}</span>
+
+                        <div className='tweet-timestamp'>
+                            {new Date(data.created_at).toLocaleTimeString()}
                         </div>
-                        
                     </div>
-                    <div className="row valign-wrapper right-align chip hoverable">
-                    {new Date(data.created_at).toLocaleTimeString()}
-                    </div>
-                    <div className="row valign-wrapper right-align chip hoverable">
-                        <a href={`https://twitter.com/${data.user.screen_name}`} target="_blank">{`@${data.user.screen_name}`}</a>
+
+                    
+                    <div className='tweet-text'>
+                        <span className='black-text'>{data.text}</span>
                     </div>
                 </div>
 
